b2a90942a49029c90035005a48a735f73e244684
Davi <davimss@gmail.com>
2021-04-22 04:01:31 -0300

    change to Debian image

---
+++ showcase-7-e-8/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu ; \
+docker rm -f showcase-7-e-8      ; \
+docker rmi -f showcase-7-e-8     ; \
+docker build -t showcase-7-e-8 . ; \
+docker run --name showcase-7-e-8 -d --restart always -p 5960:8080 showcase-7-e-8 ; \
+docker logs -f showcase-7-e-8    ; \
+docker logs -f showcase-7-e-8
